a110c9623a8b90433636e913ef2dceee56d39b9e
Kanishka Dutta <kdutta2017@outlook.com>
2024-01-18 12:04:07 -0500

    Comments

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 function add()
 {
+    //Requesting numbers from the user
     const number1 = prompt('Enter number one:');
     const number2 = prompt('Enter number two:');
+    //Using Addition to add the given numbers
     const addition = Number(number1) + Number(number2);
+    //Posting the Calculated result
     alert(`${number1} + ${number2} = ${addition}`);
 };
 function modulus()
 {
+    //Requesting numbers from the user
     const number1 = prompt('Enter number one:');
     const number2 = prompt('Enter number two:');
+    //Using Addition to add the given numbers
     const remainder = Number(number1) % Number(number2);
+    //Posting the Calculated result
     alert(`${number1} % ${number2} = ${remainder}`);
 }
